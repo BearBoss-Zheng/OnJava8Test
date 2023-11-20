87dd2a4b90f5c51ddf80978293c2bf27e9b271a2
zjx <2741630494@qq.com>
2023-11-20 14:43:45 +0800

    hot-fix first commit

--- com/zjx/test/NumberFormats.java
+++ com/zjx/test/NumberFormats.java
@@ -17,6 +17,7 @@ public class NumberFormats {
         System.out.println("Decimal: " + decimalNumber);
         System.out.println("Hexadecimal: " + hexNumber);
         System.out.println(int.class);
+        System.out.println("hot fix");
     }
 }
 
